a13f4c8ade60d8586c150975dd01b8cb561f9341
sounkou-bioinfo <sounkoutoure@gmail.com>
2025-08-03 21:03:11 +0400

    htslib/bcftools version 1.22

--- getbcftoolsSource.sh
+++ getbcftoolsSource.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/bash
-set -eufo pipefail
+set -euo pipefail
 # download bcftools from github
-version=1.21
+version=1.22
 cd $(dirname $0)/src || exit 1
+echo "$PWD"
+ls -d ${PWD}/bcftools* | xargs -I {} -t rm -rf {} || true 
+ls -d ${PWD}/htslib-* | xargs -I {} -t rm -rf {} || true
 echo "Downloading bcftools version ${version} from GitHub"
 wget https://github.com/samtools/bcftools/releases/download/${version}/bcftools-${version}.tar.bz2
 echo "Unpacking bcftools version ${version}"
@@ -10,4 +13,4 @@ tar -xjf bcftools-${version}.tar.bz2
 echo "Cleaning up"
 rm -f bcftools-${version}.tar.bz2 || true
 cd bcftools-${version} || exit 1
-mv -f htslib-{version} ../ || exit 1
+mv -f htslib-${version} ../ || exit 1
