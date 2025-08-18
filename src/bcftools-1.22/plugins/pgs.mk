93851a164f11dd6efd8eba7f3f8baf58c5c51f20
sounkou-bioinfo <sounkoutoure@gmail.com>
2025-08-18 20:40:40 +0400

    gsl configure issue

--- src/bcftools-1.22/plugins/pgs.mk
+++ src/bcftools-1.22/plugins/pgs.mk
@@ -1,14 +1,2 @@
-# The pgs plugin requires CHOLMOD library for sparse matrix operations
-# Only build if CHOLMOD is available
-ifdef HAVE_CHOLMOD
-    ifndef CHOLMOD_LIBS
-        CHOLMOD_LIBS = -lcholmod
-    endif
-    plugins/pgs.so: plugins/pgs.c
-	    $(CC) $(PLUGIN_FLAGS) $(CFLAGS) $(ALL_CPPFLAGS) $(EXTRA_CPPFLAGS) $(LDFLAGS) -o $@ version.c $< $(PLUGIN_LIBS) $(LIBS) $(CHOLMOD_LIBS)
-else
-    # Create a dummy target if CHOLMOD is not available
-    plugins/pgs.so:
-	    @echo "Warning: pgs plugin requires CHOLMOD library, skipping build"
-	    @touch $@
-endif
+plugins/pgs.so: plugins/pgs.c
+	$(CC) $(PLUGIN_FLAGS) $(CFLAGS) $(ALL_CPPFLAGS) $(EXTRA_CPPFLAGS) $(LDFLAGS) -o $@ version.c $< $(PLUGIN_LIBS) $(LIBS) -lcholmod
--- src/bcftools_version.c
+++
@@ -1,7 +0,0 @@
-#include "./bcftools-1.22/version.h"
-
-/* Minimal bcftools version function for R package */
-char *bcftools_version(void)
-{
-    return BCFTOOLS_VERSION;
-}
