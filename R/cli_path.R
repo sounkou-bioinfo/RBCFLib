df09a882bb5477303f47faa695186f59d432b743
sounkou-bioinfo <sounkoutoure@gmail.com>
2025-08-18 16:29:42 +0400

    d

--- R/cli_path.R
+++ R/cli_path.R
@@ -1,37 +1,4 @@
-#' Get path to BCFTools CLI executables
-#'
-#' Returns the path to the command-line interface executables provided by RBCFLib.
-#' These CLI tools allow users to run bcftools commands from the command line
-#' using the RBCFLib package's BCFToolsRun function.
-#'
-#' @param cli Character string; which CLI to get the path for.
-#'   Either "RBcftools" or "BCFToolsCli". Default is "RBcftools".
-#'
-#' @return Character string containing the path to the requested CLI executable
-#'
-#' @examples
-#' \dontrun{
-#' # Get path to RBcftools
-#' bcftools_cli <- BCFToolsCLIPath()
-#'
-#' # Get path to BCFToolsCli (PascalCase version)
-#' bcftools_pascal_cli <- BCFToolsCLIPath("BCFToolsCli")
-#'
-#' # Use system2 to run commands
-#' system2(bcftools_cli, c("view", "-h", vcfFile))
-#' }
-#'
-#' @export
-BCFToolsCLIPath <- function(cli = c("RBcftools", "BCFToolsCli")) {
-  cli <- match.arg(cli)
-  cli_path <- system.file("bin", cli, package = "RBCFLib")
 
-  if (cli_path == "") {
-    stop("CLI executable not found. Please reinstall RBCFLib.")
-  }
-
-  return(cli_path)
-}
 
 #' Get path to the standard bcftools binary
 #'
@@ -53,7 +20,7 @@ BCFToolsCLIPath <- function(cli = c("RBcftools", "BCFToolsCli")) {
 #'
 #' @export
 BCFToolsBinaryPath <- function() {
-  bcftools_path <- system.file("bin", "bcftools", package = "RBCFLib")
+  bcftools_path <- system.file("bin", "bin", "bcftools", package = "RBCFLib")
   
   if (bcftools_path == "") {
     stop("bcftools binary not found. Please reinstall RBCFLib.")
