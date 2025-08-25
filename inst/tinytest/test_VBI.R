d6d17f3ae532796b5fe9f940961a8ace4c44a521
sounkou-bioinfo <sounkoutoure@gmail.com>
2025-08-26 01:01:17 +0400

    inspect index

--- inst/tinytest/test_VBI.R
+++ inst/tinytest/test_VBI.R
@@ -11,10 +11,8 @@ expect_true(file.exists(vbi))
 
 # Query by region
 hits <- VBIQueryRange(vcf, vbi, "chr21:5030082-5030082")
-print(hits)
 expect_true(is.character(hits))
 
 # Query by index range
 hits2 <- VBIQueryIndex(vcf, vbi, 1, 2)
 expect_true(is.character(hits2))
-cat("Hits2:", hits2, "\n")
