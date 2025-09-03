593cfaafee6b268ace91b81aa899a0007da94be5
sounkou-bioinfo <sounkoutoure@gmail.com>
2025-09-04 03:33:21 +0400

    linking to Matrix for cholmod

--- src/bcftools-1.22/plugins/pgs.mk
+++ src/bcftools-1.22/plugins/pgs.mk
@@ -1,8 +1,8 @@
 # The pgs plugin requires CHOLMOD library for sparse matrix operations
 # Only build if CHOLMOD is available
-HAVE_CHOLMOD=no
-CHOLMOD_LIBS=
-CHOLMOD_CPPFLAGS=
+HAVE_CHOLMOD=yes
+CHOLMOD_LIBS=-L/usr/lib/R/site-library/Matrix/libs
+CHOLMOD_CPPFLAGS=-I/usr/lib/R/site-library/Matrix/libs/../include
 ifeq ($(HAVE_CHOLMOD),yes)
     plugins/pgs.so: plugins/pgs.c
 	    $(CC) $(PLUGIN_FLAGS) $(CFLAGS) $(ALL_CPPFLAGS) $(CHOLMOD_CPPFLAGS) $(EXTRA_CPPFLAGS) $(LDFLAGS) -o $@ version.c $< $(PLUGIN_LIBS) $(LIBS) $(CHOLMOD_LIBS)
